12ade7cc82a783d69c84b217404dc80e12fa7df2
Mattia46 <mattia.assogna@gmail.com>
2018-07-12 20:27:21 +0100

    step three

--- automate.js
+++ automate.js
@@ -3,44 +3,65 @@ const index = require('./index2.js');
 const arr = index.arrArrays;
 const avgTot = index.totAvg;
 const avgRow = index.rowAvg;
+let tot = [];
 
+const removeInd = (arr, arrIndex) => {
+    let index = arr.indexOf(arrIndex);
+    arr.splice(index, 1);
+}
 const removeIndex = (arr, arrIndex) => {
     let count = 0;
-    arr.map(x => {
+    tot = [];
+    let cc = arr.map(x => {
         let index = x.indexOf(arrIndex[count]);
         x.splice(index, 1);
         count++;
+        console.log('>>>', x.length);
+        return x;
     })
+    return cc;
+    console.log('asdfdfsf', cc)
 }
 
+const differ = (sumTot, ind = index.rowAvg) => {
+    //console.log('ciao', sumTot;
+    let ciao = index.rowAvg + (50 - sumTot);
+    return ciao;
+}
+
+let count = 0;
+let mattia = 50;
+let yes = 0;
 const iterate = (arr, ind = index.rowAvg) => {
-    if(arr[0].length > 0) {
-        let tot = [];
+    if(mattia > 0) {
+    //if(arr[0].length > 0) {
         let newArr = arr.map(x => {
             const left = index.closest(x, ind);
             ind = index.rowAvg - left.left;
             tot.push(x[left.ans]);
-            //x.splice(left.ans, 1);
             return x;
         })
+        console.log('qui', newArr[0].length);
+        mattia--;
         const diff = 50 - index.sum(tot);
         let lastArray = index.closest(index.e, diff);
-        tot.push(index.e[lastArray.ans])
-        let summaTot = index.sum(tot);
-        if(summaTot === 50) {
-            console.log('Yes', tot);
-            removeIndex(arr, tot);
-            //console.log('>>>>', arr);
-            //index.e.splice(lastArray.ans, 1);
+        if(diff === index.e[lastArray.ans]) {
+            yes++;
+            console.log('YES', tot);
+            let matt = removeIndex(arr, tot);
+            removeInd([index.e], lastArray.ans)
+            iterate(matt, index.rowAvg)
         } else {
-            iterate(arr, index.rowAvg - (50 - summaTot))
+            let lastArrNum = index.e[lastArray.ans];
+            let tt = ((50 - (index.sum(tot) + lastArrNum)) + index.rowAvg);
+            tot = [];
+            iterate(arr, tt);
         }
-        //console.log('sdf', diff);
-        //const last = index.closest(index.x, diff);
-        //iterate(newArr);
     }
+    console.log('YES', yes);
 }
 
+iterate(arr);
 module.exports = {
     iterate,
     arr,
