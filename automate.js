afc43d6275806d8e56c720b2ebfb2d9c5ef2214e
Mattia46 <mattia.assogna@gmail.com>
2018-07-12 19:26:36 +0100

    second step

--- automate.js
+++ automate.js
@@ -4,26 +4,40 @@ const arr = index.arrArrays;
 const avgTot = index.totAvg;
 const avgRow = index.rowAvg;
 
-const iterate = arr => {
+const removeIndex = (arr, arrIndex) => {
+    let count = 0;
+    arr.map(x => {
+        let index = x.indexOf(arrIndex[count]);
+        x.splice(index, 1);
+        count++;
+    })
+}
+
+const iterate = (arr, ind = index.rowAvg) => {
     if(arr[0].length > 0) {
         let tot = [];
         let newArr = arr.map(x => {
-            const left = index.closest(x, index.rowAvg);
+            const left = index.closest(x, ind);
+            ind = index.rowAvg - left.left;
             tot.push(x[left.ans]);
-            x.splice(left.ans, 1);
-            //console.log('>>>', x);
+            //x.splice(left.ans, 1);
             return x;
         })
         const diff = 50 - index.sum(tot);
         let lastArray = index.closest(index.e, diff);
-        console.log('>>', lastArray);
-        if(lastArray.left === 0) {
-            console.log('>>>>', arr);
-            index.e.splice(lastArray.ans, 1);
+        tot.push(index.e[lastArray.ans])
+        let summaTot = index.sum(tot);
+        if(summaTot === 50) {
+            console.log('Yes', tot);
+            removeIndex(arr, tot);
+            //console.log('>>>>', arr);
+            //index.e.splice(lastArray.ans, 1);
+        } else {
+            iterate(arr, index.rowAvg - (50 - summaTot))
         }
         //console.log('sdf', diff);
         //const last = index.closest(index.x, diff);
-        iterate(newArr);
+        //iterate(newArr);
     }
 }
 
